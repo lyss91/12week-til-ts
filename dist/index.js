633e0869f6468077657163fb00fa3001664d8278
lyss91 <leeyss1991@gmail.com>
2025-02-04 12:38:43 +0900

    [25.02.04 오전수업]

--- dist/index.js
+++ dist/index.js
@@ -1,45 +1,12 @@
-class Car {
-  name;
-  brand;
-  price;
-  year;
-  constructor(name, brand, price, year) {
-    this.name = name;
-    this.brand = brand;
-    this.price = price;
-    this.year = year;
-  }
-  move() {
-    console.log(`${this.name}을 운전합니다.`);
-  }
-  stop() {
-    console.log(`${this.name}을 멈춥니다.`);
-  }
-}
-// 상속 즉 확장을 통한 클래스 정의
-class ElectricCar extends Car {
-  // 자식 클래스에 해당하는 속성 필드
-  batteryLevel;
-  constructor(name, brand, price, year, batteryLevel) {
-    // 부모의 constructor 를 먼저 실행해 주어야 함.
-    super(name, brand, price, year);
-    this.batteryLevel = batteryLevel;
-  }
-  // 자식 클래스에 해당하는 메소드 필드
-  level() {
-    console.log(`${this.batteryLevel} 입니다.`);
-  }
+// 함수 리턴타입에서 리턴종류를 모르면
+// any 또는 unknow 을 리턴한다.
+// 만약 원하는 것이 있다면 정확히 작성해야 한다.
+
+function showValue<T>(value: T): {
+  return value;
 }
 
-// 인스턴스 생성
-let 그랜저 = new Car("현대", "그랜저", 1000, 20);
-// {name: "현대", brand: "그랜저", price: 1000, year: 20}
-그랜저.move();
-그랜저.stop();
-let 아반떼 = new Car("현대", "아반떼", 100, 30);
-// {name: "현대", brand: "아반떼", price: 100, year: 30}
-
-let 캐스퍼 = new ElectricCar("캐스퍼", "현대", 1000, 5, 100);
-캐스퍼.move();
-캐스퍼.stop();
-캐스퍼.level();
+let test = showValue(10);
+test = showValue("안녕");
+test = showValue(true);
+test = showValue([1,2,3]);
